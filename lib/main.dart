043dd0f76bad30769078828412730e01d053162f
pisyatTretiy <sqdbtw@mail.ru>
2023-11-29 02:13:52 +0300

    columns

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Columns; Khrulenko Vladislav'),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: MyColumn(),
+      ),
+    );
+  }
+}
+
+class MyColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Колонки flutter',
+          style: TextStyle(fontSize: 18.0),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          'Hello world!',
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
